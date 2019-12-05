5804224dcfda9f667eab5bf99d07849500feb6ed
Petrica Finaga <petricafinaga@gmail.com>
2019-12-05 20:30:04 +0200

    Added chat list

--- src/chat/client/ClientGUI.java
+++ src/chat/client/ClientGUI.java
@@ -1,9 +1,10 @@
 package chat.client;
 
-import java.awt.BorderLayout;
-
+import javax.swing.AbstractListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 
 public class ClientGUI extends JFrame {
@@ -15,11 +16,29 @@ public class ClientGUI extends JFrame {
 	 */
 	public ClientGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 903, 486);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JList chatList = new JList();
+		chatList.setValueIsAdjusting(true);
+		chatList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		chatList.setModel(new AbstractListModel() {
+			String[] values = new String[] { "asdasd", "asd", "asd", "as", "d", "asdasd" };
+
+			@Override
+			public int getSize() {
+				return values.length;
+			}
+
+			@Override
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		chatList.setBounds(704, 11, 171, 420);
+		contentPane.add(chatList);
+	}
 }
