a7aff34bc3c2b1f64518374577a053524c577516
Petrica Finaga <petricafinaga@gmail.com>
2019-12-05 18:41:22 +0200

    First commit

---
+++ src/chat/client/ClientAgent.java
@@ -0,0 +1,64 @@
+package chat.client;
+
+import java.awt.EventQueue;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class ClientAgent extends Agent {
+
+	private ClientGUI clientGui;
+
+	@Override
+	protected void setup() {
+
+		DFAgentDescription ad = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("chat-client");
+		sd.setName(getName());
+		sd.setOwnership("SI");
+		ad.setName(getAID());
+		ad.addServices(sd);
+
+		try {
+			// Register chat-client to DF
+			DFService.register(this, ad);
+
+			EventQueue.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						clientGui = new ClientGUI();
+						clientGui.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			ClientReceiverBehaviour receiverBehaviour = new ClientReceiverBehaviour(this);
+			this.addBehaviour(receiverBehaviour);
+
+		} catch (FIPAException e) {
+			doDelete();
+		}
+	}
+
+	@Override
+	public void takeDown() {
+		try {
+			DFService.deregister(this);
+			clientGui.dispose();
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void OnMessage(String message, AID sender) {
+		System.out.println(message + " " + sender.getName());
+	}
+}
---
+++ src/chat/client/ClientReceiverBehaviour.java
@@ -0,0 +1,26 @@
+package chat.client;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class ClientReceiverBehaviour extends CyclicBehaviour {
+
+	private ClientAgent myAgent;
+
+	public ClientReceiverBehaviour(ClientAgent clientAgent) {
+		super(clientAgent);
+		myAgent = clientAgent;
+	}
+
+	@Override
+	public void action() {
+		ACLMessage message = myAgent.receive();
+
+		if (null != message) {
+			myAgent.OnMessage(message.getContent(), message.getSender());
+		} else {
+			block();
+		}
+	}
+
+}
