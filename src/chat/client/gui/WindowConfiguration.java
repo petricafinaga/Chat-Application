3f25d6479e1845f6cc44b838df8ec7a1a0299e4a
Petrica Finaga <petricafinaga@gmail.com>
2020-01-21 09:44:34 +0200

    Last commit

--- src/chat/client/gui/WindowConfiguration.java
+++ src/chat/client/gui/WindowConfiguration.java
@@ -1,3 +1,10 @@
+/**
+ * @author Acroitoritei Calin
+ *
+ * @version 1.0
+ * @since 05-12-2019
+ **/
+
 package chat.client.gui;
 
 import java.awt.Color;
@@ -16,50 +23,48 @@ public class WindowConfiguration {
 		this.myMessageColor = Color.black;
 		this.fontSize = 14;
 	}
-	
-	
+
 	// My message color
 	public void setMyMessageColor(Color _myMsgColor) {
 		this.myMessageColor = _myMsgColor;
 	}
-	
+
 	public Color getMyessageColor() {
 		return this.myMessageColor;
 	}
-	
+
 	// Received message color
 	public void setReceivedMessageColor(Color _recvMsgColor) {
 		this.receivedMessageColor = _recvMsgColor;
 	}
-	
+
 	public Color getReceivedMessageColor() {
 		return this.receivedMessageColor;
 	}
-	
+
 	// Window color
 	public void setWindowColor(Color _windowColor) {
 		this.windowColor = _windowColor;
 	}
-	
+
 	public Color getWindowColor() {
 		return this.windowColor;
 	}
-	
+
 	// Font size
 	public void setFontSize(int _fontSize) {
 		this.fontSize = _fontSize;
 	}
-	
-	public int getFontSize()
-	{
+
+	public int getFontSize() {
 		return this.fontSize;
 	}
-	
+
 	// Font name
 	public void setFontName(String _fontName) {
 		this.fontName = _fontName;
 	}
-	
+
 	public String getFontName() {
 		return this.fontName;
 	}
